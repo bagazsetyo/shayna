5b163ccd0e68249a77d5ae3e05773b0b445a0b45
bagazsetyo <bagazfrazetyo@gmail.com>
2020-04-13 12:03:43 +0700

    upload project veu

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+     <Headershayna />
+     <HeroCarousel />
+     <Banner />
+     <Insta />
+     <LogoPartner />
+     <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Headershayna from '@/components/Headershayna.vue'
+import HeroCarousel from '@/components/HeroCarousel.vue'
+import Banner from '@/components/Banner.vue'
+import Insta from '@/components/Insta.vue'
+import LogoPartner from '@/components/LogoPartner.vue'
+import Footer from '@/components/Footer.vue'
+
+// import carousel from 'vue-owl-carousel'
+
+
+export default {
+  name: 'Home',
+  components: {
+    Headershayna,
+    HeroCarousel,
+    Banner,
+    Insta,
+    LogoPartner,
+    Footer
+    // carousel
+  }
+}
+</script>
